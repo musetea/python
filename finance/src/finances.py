37711a28c929876415fe48fb57862e517c9e828f
musetea <dev0019@naver.com>
2022-06-24 07:53:32 +0900

    ing

--- finance/src/finances.py
+++ finance/src/finances.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import FinanceDataReader as fdr
+import numpy as np
 
 def krx():
     krx = fdr.StockListing("KRX")
@@ -25,14 +26,27 @@ def fn_ex(d):
     print(d.loc[[0,1,2,3], ['Name', 'Symbol']])
     print(d[d['Name'] == '카카오'])
 
+# 서울소재지 코스피 종목의 종목코드 와 종목명만 수집 
 def fn_region_market(d):
-    print(d[ d['Region'] == "서울특별시" & d['Market'] == "KOSPI" ] )
+    print("서울소재지 코스피 종목의 종목코드 와 종목명만 수집")
+    rows = (d['Region'] == "서울특별시") & (d['Market'] == "KOSPI")
+    print(d.loc[rows, ["Symbol", "Name"]] )
+
+## 결측치
+def fn_listingDate(d):
+    print(d["ListingDate"].head(1))
+    d['ListingYear'] = d['ListingDate'].dt.year
+    print(d['ListingYear'])
+    # np.nan == float
+    print(type(np.nan))
     
 
 def main():
     d = pd.read_csv('krx.csv')
-    fn_region_market(d)
-    
+    # object -> datetime
+    d["ListingDate"] = pd.to_datetime(d['ListingDate'])
+    #fn_region_market(d)
+    fn_listingDate(d)    
 
 if __name__ == "__main__":
     main()
